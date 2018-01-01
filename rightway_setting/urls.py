d61d08f256c8e281590bd3818b57ad821d9884e8
vishalmanani <vishal@lintelindia.com>
2018-01-01 23:59:32 +0530

    change in url

--- rightway_setting/urls.py
+++ rightway_setting/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^admin', admin.site.urls),
     url(r'^bigcommerce/callback', views.AuthCallback.as_view(), name='authcallback'),
     url(r'^bigcommerce/load', views.Load.as_view(), name='load'),
-    # url(r'^', views.Display.as_view(), name='display'),
     url(r'^store/$', views.DisplayStoreUser.as_view(), name='store'),
+    url(r'^', views.Display.as_view(), name='display'),
 ]
